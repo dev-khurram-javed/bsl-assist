7c142ba1640d2a1c0ecfab4c4cfee0dab2c92398
Khurram Javed <dev.khurram.javed@gmail.com>
2024-11-08 01:39:49 +0500

    Style changes

--- wp-content/themes/bsl-assist/app/components/headline/headline.php
+++ wp-content/themes/bsl-assist/app/components/headline/headline.php
@@ -2,7 +2,7 @@
 
 wp_register_component('Headline', function($data) {
 
-    if (!isset($data['text'])) return;
+    if (!isset($data['text']) || $data['text'] == null || empty($data['text'])) return;
 
     printf('<%s>%s</%s>', $data['html_tag'], $data['text'], $data['html_tag']);
 }, [
